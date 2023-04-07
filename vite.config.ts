b46d9fcb7ae2805e7ecfd0e1f97d7501cb2547f8
Esteban Salazar Boamorte <salazar.z.esteban@gmail.com>
2023-04-07 12:18:55 -0500

    fix(project): :bug: fix github deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-const isProd = process.env.NODE_ENV === 'production';
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: isProd ? 'https://boamorte8.github.io/gh-react-vite/' : '',
-  plugins: [tsconfigPaths(), react()],
+export default defineConfig(({ command }) => {
+  const base = command === 'build' ? 'https://boamorte8.github.io/gh-react-vite/' : '';
+  return {
+    base,
+    plugins: [tsconfigPaths(), react()],
+  };
 });
