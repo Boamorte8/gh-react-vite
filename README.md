c72316e52db7d06c2b14d731c9001ac775cdf93c
Esteban Salazar Boamorte <salazar.z.esteban@gmail.com>
2023-04-07 10:07:34 -0500

    build(project): :wrench: add eslint and stylelint

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # gh-react-vite
 
-This a project to learn and put on practice how to add and setup github actions in a project
+This a project to learn and put on practice the next topics:
+
+- How to add and setup github actions in a project
+- Practice how to configure babel on a react project
+- Test [tremor](https://www.tremor.so) library
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://boamorte8.github.io/gh-react-vite/",
+  base: 'https://boamorte8.github.io/gh-react-vite/',
   plugins: [react()],
 });
