7e71079a5d48128212a5eb3cdbb4d0d3a21445be
Esteban Salazar Boamorte <salazar.z.esteban@gmail.com>
2023-04-07 12:53:06 -0500

    feat(project): :sparkles: add new page to test router

--- src/routes/RootRouter.tsx
+++ src/routes/RootRouter.tsx
@@ -1,11 +1,16 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import { Home } from './Home';
+import { Home } from './Home/Home';
+import { Test } from './Test';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
+  },
+  {
+    path: '/gh-react-vite',
+    element: <Test />,
   },
 ]);
 
---
+++ src/routes/Test.tsx
@@ -0,0 +1,5 @@
+export const Test = () => (
+  <div>
+    <h1>Test page on route gh-react-vite</h1>
+  </div>
+);
