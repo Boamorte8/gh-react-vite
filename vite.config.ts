51212d3f988086c67662e9a571e9732645265d5a
Esteban Salazar Boamorte <salazar.z.esteban@gmail.com>
2023-04-07 11:44:04 -0500

    feat(project): :sparkles: add router and create header and home page

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+const isProd = process.env.NODE_ENV === 'production';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://boamorte8.github.io/gh-react-vite/',
-  plugins: [react()],
+  base: isProd ? 'https://boamorte8.github.io/gh-react-vite/' : '',
+  plugins: [tsconfigPaths(), react()],
 });
